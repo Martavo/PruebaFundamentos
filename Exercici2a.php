99f5b13f9f7df750264cefa62afd98209acb4f23
Marta Villalba <marta.villalba.orta@gmail.com>
2023-11-30 09:59:21 +0100

    Correcciones

--- Exercici2a.php
+++ Exercici2a.php
@@ -1,24 +1,18 @@
 <?php
-$peso = 65;
-$altura = 168;
+$peso = readline("Dame el peso: ");
+$altura = readline("Dame la altura: ");
 
-echo calcularIMC($peso, $altura);
+$IMC = $peso / ($altura * 2);
 
-public function calcularIMC($peso, $altura){
-    $IMC = $peso/altura*0.25;
-
-    if ($IMC < 18.5){
-       return echo "Tu IMC está por debajo de lo normal";
-    }ifelse ($IMC >= 18.6 || $IMC > 24.9 ) {
-        return echo "Tu IMC és normal";
-    } ifelse ($IMC >= 25 || $IMC > 29.9 ){
-        return echo "Tu IMC corresponde con sobrepreso";
-    }ifelse ($IMC >= 30){
-        return echo "Tu IMC corresponde con obesidad";
-    }else{
-        return echo "El valor indicado no es correcto";
-    }
+if ($IMC < 18.5) {
+    echo "Tu IMC está por debajo de lo normal";
+} elseif ($IMC <= 24.9) {
+    echo "Tu IMC es normal";
+} elseif ($IMC <= 29.9) {
+    echo "Tu IMC corresponde con sobrepeso";
+} elseif ($IMC <= 30) {
+    echo "Tu IMC corresponde con obesidad";
+} else {
+    echo "El valor indicado no es correcto";
 }
-
-
 ?>
